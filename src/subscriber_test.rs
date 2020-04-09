369094da975379b2ed0dceab22b70e2b0e2eda07
sagie gur ari <sagiegurari@gmail.com>
2020-04-09 22:24:37 +0000

    redis upgrade

--- src/subscriber_test.rs
+++ src/subscriber_test.rs
@@ -3,7 +3,6 @@ use super::*;
 #[test]
 fn create_check_state() {
     let subscriber = create();
-    assert!(!subscriber.subscribed);
     assert_eq!(subscriber.subscriptions.len(), 0);
     assert_eq!(subscriber.psubscriptions.len(), 0);
     assert!(subscriber.redis_connection.is_none());
